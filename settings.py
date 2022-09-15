d8c76ff9f5e1749dab93ce452301ab5c153edfcd
Yann Piquet <zaeffy@DESKTOP-19P8STK.localdomain>
2022-09-15 21:55:48 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    debug: bool = False
+    
+    api_key: str = 'invalid_key'
+
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
